bbd0b8777d5dff56716a3fdc2a9c9d8467510d98
Susan Quiros <squiros@houstonfoodbank.org>
2021-06-25 20:34:18 -0500

    working on app.py

--- app.py
+++ app.py
@@ -4,3 +4,20 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+Base = automap_base ()
+
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+#getting the link
+session = Session(engine)
+
+#calling the flask
+app = Flask(__name__)
