be42cc1567e70651daea733f28d6b9711ff6b7c4
Susan Quiros <squiros@houstonfoodbank.org>
2021-06-27 18:06:46 -0500

    finishing the homework 1st part

--- app.py
+++ app.py
@@ -4,16 +4,14 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 from flask import Flask, jsonify
-
 #################################################
 # Database Setup
 #################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
 Base = automap_base ()
-
-Base.prepare(engine, reflect=True)
-
+#reflecting the tables
+Base.prepare(engine,reflect=True)
+#connecting with each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 #getting the link
@@ -21,3 +19,19 @@ session = Session(engine)
 
 #calling the flask
 app = Flask(__name__)
+
+@app.route("/")
+def Home():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br>"
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
